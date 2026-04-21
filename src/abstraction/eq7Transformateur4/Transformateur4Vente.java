861a25ec91cd73b7af82e9a39f900d2348dfd519
Aymeric Hostyn <hostyn.aymeric@gmail.com>
2026-04-21 11:36:03 +0200

    push

--- src/abstraction/eq7Transformateur4/Transformateur4Vente.java
+++ src/abstraction/eq7Transformateur4/Transformateur4Vente.java
@@ -1,7 +1,47 @@
 package abstraction.eq7Transformateur4;
 
+import abstraction.eqXRomu.contratsCadres.Echeancier;
+import abstraction.eqXRomu.contratsCadres.ExemplaireContratCadre;
+import abstraction.eqXRomu.contratsCadres.IVendeurContratCadre;
+import abstraction.eqXRomu.produits.IProduit;
 
 //Auteur -> Aymeric
 public class Transformateur4Vente extends Transformateur4Production implements IVendeurContratCadre {
+
+    @Override
+    public boolean vend(IProduit produit) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'vend'");
+    }
+
+    @Override
+    public Echeancier contrePropositionDuVendeur(ExemplaireContratCadre contrat) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'contrePropositionDuVendeur'");
+    }
+
+    @Override
+    public double propositionPrix(ExemplaireContratCadre contrat) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'propositionPrix'");
+    }
+
+    @Override
+    public double contrePropositionPrixVendeur(ExemplaireContratCadre contrat) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'contrePropositionPrixVendeur'");
+    }
+
+    @Override
+    public void notificationNouveauContratCadre(ExemplaireContratCadre contrat) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'notificationNouveauContratCadre'");
+    }
+
+    @Override
+    public double livrer(IProduit produit, double quantite, ExemplaireContratCadre contrat) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'livrer'");
+    }
     
-   
+}
