5b5aba8c77836fe3b3d6671fe2a96d528797ef12
Thomas Croizé <thomas.croize@imt-atlantique.net>
2026-05-05 11:36:54 +0200

    MAJ indicateurs

--- src/abstraction/eq2Producteur2/Producteur2Acteur.java
+++ src/abstraction/eq2Producteur2/Producteur2Acteur.java
@@ -79,8 +79,8 @@ public class Producteur2Acteur extends Producteur2couts implements IActeur {
 		// Stock total général
 		res.add(this.stockTotal);
 		
-		// Stocks détaillés par type de fève
-		for (Feve feve : Feve.values()) {
+		// Stocks détaillés par type de fève (hors BQ_E et MQ_E)
+		for (Feve feve : new Feve[] {Feve.F_HQ, Feve.F_BQ, Feve.F_MQ, Feve.F_HQ_E}) {
 			Variable stockFeve = this.stockvar.get(feve);
 			if (stockFeve != null) {
 				res.add(stockFeve);
